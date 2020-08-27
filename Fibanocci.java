14897ae73ac3276a735ee35dd436966d368faa4b
Naveen Karampuri <65078185+Naveen2224@users.noreply.github.com>
2020-08-27 06:13:37 -0700

    creat Fibanocci.java

---
+++ Fibanocci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Fibanocci {
+    public static void main(String[] args){
+        int n ;
+        int a=0,b=1,c;
+        System.out.println("enter the no : ");
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        System.out.println("the fibanocci series is : ");
+        System.out.print(a+" "+b);
+        for(int i=2;i<n;i++){
+            c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
+
+
+
+
+
+#OUTPUT: 
+enter the no : 
+10
+the fibanocci series is : 
+0 1 1 2 3 5 8 13 21 34
